640c9f8bf27500d4a6ddbba83e5ac98c3a7eff12
MonwF <boluo2@gmail.com>
2021-07-27 22:03:22 +0800

    remove win32 support

--- extract.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-pushd %~dp0
-if not exist tools\win32\ (
-    tools\win32-tools.exe -otools -y
-)
-call tools\win32-addons\add.bat > NUL
-tools\win32\bin\bash.exe -c 'PATH=$PWD/tools/win32/bin bash extract.sh %*'
-pause
